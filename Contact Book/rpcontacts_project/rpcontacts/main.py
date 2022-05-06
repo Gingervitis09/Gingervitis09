bb1062576736a681ba61e04de5cae4742459134a
Gingervitis09 <105084901+Gingervitis09@users.noreply.github.com>
2022-05-06 18:28:23 -0400

    Add files via upload

---
+++ Contact Book/rpcontacts_project/rpcontacts/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# rpcontacts/main.py
+
+"""This module provides RP Contacts application."""
+
+import sys
+
+from .views import Window
+
+def main():
+    """RP Contacts main function."""
+    # Create the application
+    app = QApplication(sys.argv)
+    #Create the main window
+    win = Window()
+    win.show()
+    #Run the event loop
+    sys.exit(app.exec())
